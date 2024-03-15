8dd7ecfa3076e4ba02dc3d860798ce267c3936b8
KeeHeung <137737873+KeeHeung@users.noreply.github.com>
2024-03-15 23:54:15 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int equalNum = 1;
+        int diffNum = -1;
+        
+        if (num1 == num2) {
+            return equalNum;
+        } else {
+            return diffNum;    
+        }
+        
+    }
+}
