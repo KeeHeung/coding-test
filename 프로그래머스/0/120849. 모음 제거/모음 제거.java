0e135f071203a64d47330aff4aee80b93f4921e8
KeeHeung <137737873+KeeHeung@users.noreply.github.com>
2024-04-05 10:03:02 +0900

    [level 0] Title: 모음 제거, Time: 0.10 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,12 +1,5 @@
 class Solution {
-    public String solution(String my_string) {
-		if(my_string.contains("a") || my_string.contains("e") || my_string.contains("i") || my_string.contains("o") || my_string.contains("u")) {
-			my_string = my_string.replace("a","");
-			my_string = my_string.replace("e","");
-			my_string = my_string.replace("i","");
-			my_string = my_string.replace("o","");
-			my_string = my_string.replace("u","");
-		}        
-        return my_string;
+    public String solution(String my_string) { 
+        return my_string.replaceAll("[aeiou]", "");
     }
 }
