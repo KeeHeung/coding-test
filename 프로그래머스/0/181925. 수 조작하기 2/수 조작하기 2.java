635b2466b071875b12aff31075b6d2d9fc6cee5a
KeeHeung <137737873+KeeHeung@users.noreply.github.com>
2024-04-08 00:33:24 +0900

    [level 0] Title: 수 조작하기 2, Time: 695.07 ms, Memory: 382 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,7 +1,6 @@
 class Solution {
     public String solution(int[] numLog) {
         String answer = "";
-        int idx = numLog.length;
         for(int i = 0; i < numLog.length-1; i++) {
             if (numLog[i+1] - numLog[i] == 1) answer += "w";
             if (numLog[i+1] - numLog[i] == -1) answer += "s";
