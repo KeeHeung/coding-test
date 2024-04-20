cf27830ead5297e81ee8e0e47aed57d70b8aa10c
KeeHeung <137737873+KeeHeung@users.noreply.github.com>
2024-04-20 17:51:11 +0900

    [level 0] Title: 세 개의 구분자, Time: 91.16 ms, Memory: 106 MB -BaekjoonHub

---
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String myStr) {
+        myStr = myStr.replaceAll("[a|b|c]+", ",");
+		myStr = myStr.charAt(0) == ',' ? myStr.substring(1) : myStr;
+		myStr = myStr.equals("") ? "EMPTY" : myStr;
+		
+		return myStr.split("[,]");
+    }
+}
