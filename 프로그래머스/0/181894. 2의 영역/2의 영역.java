f0f821ed8cf05dbd93225f7be308994be71f53d9
KeeHeung <137737873+KeeHeung@users.noreply.github.com>
2024-04-20 19:50:20 +0900

    [level 0] Title: 2의 영역, Time: 1.22 ms, Memory: 100 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import static java.util.Arrays.copyOfRange;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        
+        int min = 100000, max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 2) {
+                min = Math.min(min, i);
+                max = Math.max(max, i);
+            }
+        }
+
+        if (min <= max) {
+            return Arrays.copyOfRange(arr, min, max + 1);
+        } else {
+            return new int[]{-1};
+        }
+        
+    }
+}
