ba4d04acc4cc795bb5702ac868e366f635d13086
KeeHeung <137737873+KeeHeung@users.noreply.github.com>
2024-04-30 22:11:50 +0900

    [level 0] Title: 암호 해독, Time: 11.80 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+		for (int i = 0; i < cipher.length()+1; i++) {
+			if (i % code == 0 && i != 0)
+				answer += cipher.charAt(i-1);
+		}
+        return answer;
+    }
+}
