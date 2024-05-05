e111552ad483e8622030e30738e228a4723c822d
KeeHeung <137737873+KeeHeung@users.noreply.github.com>
2024-05-05 22:49:03 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 147.76 ms, Memory: 100 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int k) {
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = 0; i < arr.length; i++) {
+			if (!list.contains(arr[i])) list.add(arr[i]);
+		}
+
+		int[] answer = new int[k];
+		for (int i = 0; i < answer.length; i++) {
+			if (i >= list.size()) answer[i] = -1;
+			if (i < list.size()) answer[i] = list.get(i);
+		}
+        
+        return answer;
+    }
+}
