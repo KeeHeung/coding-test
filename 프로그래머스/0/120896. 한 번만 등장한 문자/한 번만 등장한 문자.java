d07d4e4b684ac5a8a16f165661ad821b102beb09
KeeHeung <137737873+KeeHeung@users.noreply.github.com>
2024-05-13 21:52:31 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.44 ms, Memory: 67.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+		char[] sList = s.toCharArray();
+
+		String answer = "";
+		ArrayList<Character> charArray = new ArrayList<>();
+		for (int i = 0; i < sList.length; i++) {
+			int cnt = 0;
+			for (int j = 0; j < sList.length; j++) {
+				if (sList[i] == sList[j]) cnt++;
+			}
+			if (cnt == 1) charArray.add(sList[i]);
+		}
+
+		char[] cList = new char[charArray.size()];
+		for (int i = 0; i < charArray.size(); i++) {
+			cList[i] = charArray.get(i);
+		}
+		Arrays.sort(cList);
+        
+        return new String(cList);
+    }
+}
